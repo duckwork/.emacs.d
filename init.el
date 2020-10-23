faada2995a3f98c33b3a2364e91f0d2db3cd9f5a
Case Duckworth <acdw@acdw.net>
2020-10-22 23:35:26 -0500

    Add encoding

--- init.el
+++ init.el
@@ -34,6 +34,15 @@
   :config
   (recentf-mode +1))
 
+;; encoding
+(set-charset-priority 'unicode)
+(set-language-environment "UTF-8")
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+
 ;;; Save/Restore
 ;; save places in files
 (use-package saveplace
