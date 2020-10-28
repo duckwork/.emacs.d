d9bf322b0b89411d2f35c0e411d494f08973757d
Case Duckworth <acdw@acdw.net>
2020-10-27 21:18:49 -0500

    Unignore var/elpher-bookmarks.el

---
+++ var/elpher-bookmarks.el
@@ -0,0 +1,8 @@
+; Elpher bookmarks file
+
+; Bookmarks are stored as a list of (label URL) items.
+; Feel free to edit by hand, but take care to ensure
+; the list structure remains intact.
+
+(("Spacewalk" "gemini://rawtext.club/~sloum/spacewalk.gmi")
+ ("CAPCOM" "gemini://gemini.circumlunar.space/capcom/"))
