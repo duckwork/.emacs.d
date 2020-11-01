166ef038af6e578202a79fcd689cee9774fb5afa
Case Duckworth <acdw@acdw.net>
2020-11-01 00:24:18 -0500

    Add bookmarks

--- var/elpher-bookmarks.el
+++ var/elpher-bookmarks.el
@@ -4,5 +4,6 @@
 ; Feel free to edit by hand, but take care to ensure
 ; the list structure remains intact.
 
-(("Spacewalk" "gemini://rawtext.club/~sloum/spacewalk.gmi")
+(("gem.acdw.net" "gemini://gem.acdw.net/")
+ ("Spacewalk" "gemini://rawtext.club/~sloum/spacewalk.gmi")
  ("CAPCOM" "gemini://gemini.circumlunar.space/capcom/"))
