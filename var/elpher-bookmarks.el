b13434ce36a86c337198bcf14b78108508830b95
Case Duckworth <acdw@acdw.net>
2020-11-19 20:20:12 -0600

    Add bookmark

--- var/elpher-bookmarks.el
+++ var/elpher-bookmarks.el
@@ -4,7 +4,8 @@
 ; Feel free to edit by hand, but take care to ensure
 ; the list structure remains intact.
 
-(("weechat relay" "gemini://low-key.me/guides/weechat_irc_relay.gmi")
+(("gemini://breadpunk.club" "gemini://breadpunk.club/")
+ ("weechat relay" "gemini://low-key.me/guides/weechat_irc_relay.gmi")
  ("cosmic voyage" "gemini://cosmic.voyage/")
  ("kayw" "gemini://salejandro.me/")
  ("gem.acdw.net" "gemini://gem.acdw.net/")
