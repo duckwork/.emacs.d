d2627a3cb65035cdb6f99ab2da3956b0960455af
Case Duckworth <acdw@acdw.net>
2020-11-25 22:09:58 -0600

    Re-organize elpher bookmarks

--- var/elpher-bookmarks.el
+++ var/elpher-bookmarks.el
@@ -4,10 +4,10 @@
 ; Feel free to edit by hand, but take care to ensure
 ; the list structure remains intact.
 
-(("gemini://breadpunk.club" "gemini://breadpunk.club/")
- ("weechat relay" "gemini://low-key.me/guides/weechat_irc_relay.gmi")
+(("BREADPUNK!" "gemini://breadpunk.club/")
+ ("ACDW" "gemini://gem.acdw.net/")
+ ("Spacewalk" "gemini://rawtext.club/~sloum/spacewalk.gmi")
+ ("CAPCOM" "gemini://gemini.circumlunar.space/capcom/")
  ("cosmic voyage" "gemini://cosmic.voyage/")
  ("kayw" "gemini://salejandro.me/")
- ("gem.acdw.net" "gemini://gem.acdw.net/")
- ("Spacewalk" "gemini://rawtext.club/~sloum/spacewalk.gmi")
- ("CAPCOM" "gemini://gemini.circumlunar.space/capcom/"))
+ ("weechat relay" "gemini://low-key.me/guides/weechat_irc_relay.gmi"))
