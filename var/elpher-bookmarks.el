1f97233df5e0434eaef213238da0a442b3d50a3b
Case Duckworth <acdw@acdw.net>
2020-11-29 00:37:46 -0600

    Add bookmark

--- var/elpher-bookmarks.el
+++ var/elpher-bookmarks.el
@@ -4,7 +4,8 @@
 ; Feel free to edit by hand, but take care to ensure
 ; the list structure remains intact.
 
-(("BREADPUNK!" "gemini://breadpunk.club/")
+(("Caolan - Vermont Sourdough" "gemini://caolan.uk/baking/2020-11-26_vermont_sourdough.gmi")
+ ("BREADPUNK!" "gemini://breadpunk.club/")
  ("ACDW" "gemini://gem.acdw.net/")
  ("Spacewalk" "gemini://rawtext.club/~sloum/spacewalk.gmi")
  ("CAPCOM" "gemini://gemini.circumlunar.space/capcom/")
