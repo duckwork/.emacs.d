3f1cd96a63c6fd372782e79af2f97659cf4725ff
Case Duckworth <acdw@acdw.net>
2020-12-21 23:20:21 -0600

    Update README

--- init.el
+++ init.el
@@ -1,5 +1,9 @@
 ;; init.el -*- lexical-binding: t -*-
 
+(setq gc-cons-threshold most-positive-fixnum)
+(defvar old-file-name-handler file-name-handler-alist)
+(setq file-name-handler-alist nil)
+
 (let* ((conf (expand-file-name "config"
 			       user-emacs-directory))
        (conf-el (concat conf ".el"))
@@ -8,3 +12,7 @@
 	       (load conf 'no-error))
     (require 'org)
     (org-babel-load-file conf-org)))
+
+(setq gc-cons-threshold 16777216 ; 16mb
+      gc-cons-percentage 0.1
+      file-name-handler-alist old-file-name-handler)
