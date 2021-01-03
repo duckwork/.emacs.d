865a9a218af3fd01756712c3d378e47d536ca785
Case Duckworth <acdw@acdw.net>
2021-01-03 08:38:17 -0600

    Add newer Org to load-path before tangling
    This fixes a problem where, if config.org was newer than config.el, Emacs would
    require the Org that comes with Emacs, which is older and breaks exporting, at least
    to Markdown.  Adding the newer Org to the load path loads the newer Org instead.

--- init.el
+++ init.el
@@ -12,5 +12,8 @@
        (conf-org (concat conf ".org")))
   (unless (and (file-newer-than-file-p conf-el conf-org)
 	       (load conf 'no-error))
+    ;; A plain require here just loads the older `org' in Emacs' install dir.  We
+    ;; need to add the newer one to the `load-path', hopefully that's all.
+    (add-to-list 'load-path (expand-file-name "straight/build/org/"))
     (require 'org)
     (org-babel-load-file conf-org)))
