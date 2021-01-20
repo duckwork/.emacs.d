093e18808c3a65647aa015f71b4bb47cab98c64e
Case Duckworth <acdw@acdw.net>
2021-01-19 23:40:14 -0600

    Start over ... again

--- early-init.el
+++ early-init.el
@@ -1,9 +1,53 @@
 ;; early-init.el -*- no-byte-compile: t; -*-
+;; This file is automatically tangled from config.org.
+;; Hand edits will be overwritten!
 
-;; I use `straight.el' instead of `package.el'.
+;;  Disable loading of =package.el=
+
+;; I use =straight.el= instead.
+
+
+;; [[file:~/.config/emacs/config.org::*Disable loading of =package.el=][Disable loading of =package.el=:1]]
 (setq package-enable-at-startup nil)
+;; Disable loading of =package.el=:1 ends here
 
 ;; Don't resize the frame when loading fonts
+
+
+;; [[file:~/.config/emacs/config.org::*Don't resize the frame when loading fonts][Don't resize the frame when loading fonts:1]]
 (setq frame-inhibit-implied-resize t)
+;; Don't resize the frame when loading fonts:1 ends here
+
 ;; Resize frame by pixels
+
+
+;; [[file:~/.config/emacs/config.org::*Resize frame by pixels][Resize frame by pixels:1]]
 (setq frame-resize-pixelwise t)
+;; Resize frame by pixels:1 ends here
+
+;; Shoe-horned from elsewhere in =config.org=
+
+;; A fundamental tension of literal programming is logical versus
+;; programmatic ordering.  I understand that's a problem it's meant to
+;; solve but hey, maybe I'm not quite there yet.  I feel that having this
+;; weird shoe-horning of other bits of my config here, in a backwater
+;; heading in an appendix, isn't quite the future I wanted.  But it's
+;; what I have for now.
+
+
+;; [[file:~/.config/emacs/config.org::*Shoe-horned from elsewhere in =config.org=][Shoe-horned from elsewhere in =config.org=:1]]
+(add-to-list 'default-frame-alist
+	     '(tool-bar-lines . 0))
+
+(tool-bar-mode -1)
+(add-to-list 'default-frame-alist
+	     '(menu-bar-lines . 0))
+
+(menu-bar-mode -1)
+(add-to-list 'default-frame-alist
+	     '(vertical-scroll-bars . nil)
+	     '(horizontal-scroll-bars . nil))
+
+(scroll-bar-mode -1)
+(horizontal-scroll-bar-mode -1)
+;; Shoe-horned from elsewhere in =config.org=:1 ends here
--- init.el
+++ init.el
@@ -1,21 +1,37 @@
 ;; init.el -*- lexical-binding: t -*-
+;; This file is automatically tangled from config.org.
+;; Hand edits will be overwritten!
 
+;; Prefer newer files to older files
+
+
+;; [[file:~/.config/emacs/config.org::*Prefer newer files to older files][Prefer newer files to older files:1]]
 (setq load-prefer-newer t)
+;; Prefer newer files to older files:1 ends here
 
+;; Load the config
+
+;; I keep most of my config in =config.el=, which is tangled directly
+;; from this file.  This init just loads that file, either from lisp or
+;; directly from Org if it's newer.
+
+
+;; [[file:~/.config/emacs/config.org::*Load the config][Load the config:1]]
 (let* (;; Speed up init
        (gc-cons-threshold most-positive-fixnum)
        (file-name-handler-alist nil)
        ;; Config file names
        (conf (expand-file-name "config"
-			       user-emacs-directory))
+                               user-emacs-directory))
        (conf-el (concat conf ".el"))
        (conf-org (concat conf ".org")))
   (unless (and (file-newer-than-file-p conf-el conf-org)
-	       (load conf 'no-error))
+               (load conf 'no-error))
     ;; A plain require here just loads the older `org'
     ;; in Emacs' install dir.  We need to add the newer
     ;; one to the `load-path', hopefully that's all.
     (add-to-list 'load-path (expand-file-name "straight/build/org"
-					      user-emacs-directory))
+                                              user-emacs-directory))
     (require 'org)
     (org-babel-load-file conf-org)))
+;; Load the config:1 ends here
