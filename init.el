6f7da55585ecb39e6e0d302b1bab2ac67063afb2
Case Duckworth <acdw@acdw.net>
2021-01-21 23:17:28 -0600

    2020-01-21

--- init.el
+++ init.el
@@ -39,16 +39,12 @@ If COMMANDS is empty or nil, simply return the result of CONDITIONS."
        (config.org (concat config ".org"))
        (straight-org-dir (expand-file-name "straight/build/org"
                                            user-emacs-directory)))
-       ;; Unless config.org is /newer/ than config.el, *or* the config
-       ;; is able to be loaded without errors, load the config from
-       ;; config.org.
-       (unless (or (file-newer-than-file-p config.org config.el)
-                   (load config 'no-error))
-         ;; A plain require here just loads the older `org'
-         ;; in Emacs' install dir.  We need to add the newer
-         ;; one to the `load-path', hopefully that's all.
-         (when (file-exists-p straight-org-dir)
-           (add-to-list 'load-path straight-org-dir))
-         ;; Load config.org
-         (require 'org)
-         (org-babel-load-file config.org)))
+  (unless (load config 'no-error))
+  ;; A plain require here just loads the older `org'
+  ;; in Emacs' install dir.  We need to add the newer
+  ;; one to the `load-path', hopefully that's all.
+  (when (file-exists-p straight-org-dir)
+    (add-to-list 'load-path straight-org-dir))
+  ;; Load config.org
+  (require 'org)
+  (org-babel-load-file config.org :compile))
